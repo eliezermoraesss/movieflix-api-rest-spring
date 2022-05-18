3b0ebe34b94fe924678ad27823637224f0404b25
eliezermoraesss <eliezer.moraes@outlook.com>
2022-05-18 11:46:15 -0300

    Fix: Included @DeleteMapping in GenreResource

--- src/main/java/com/devsuperior/movieflix/resources/GenreResource.java
+++ src/main/java/com/devsuperior/movieflix/resources/GenreResource.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -48,6 +49,7 @@ public class GenreResource {
 		return ResponseEntity.ok().body(dto);
 	}
 	
+	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<GenreDTO> delete(@PathVariable Long id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
